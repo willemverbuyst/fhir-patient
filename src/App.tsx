97c8f6d4aed486d323e4f59b5325548f4132910c
Willem Verbuyst <willemverbuyst@gmail.com>
2024-05-09 05:37:24 +0200

    Use fhir types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Patient } from "fhir/r4";
+import patientJson from "../patient.json";
 import "./App.css";
 
 function App() {
-  return <h1>Hello world</h1>;
+  const patient = patientJson as Patient;
+
+  return (
+    <section>
+      <h1>
+        {patient.name
+          ?.map((i) => i.given?.join(", ") + " " + i.family)
+          .join(", ")}
+      </h1>
+      <p>{patient.birthDate}</p>
+    </section>
+  );
 }
 
 export default App;
