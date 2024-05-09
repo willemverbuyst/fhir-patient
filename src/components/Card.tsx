2455f494d2559c8fbd8107dd15e87d10770787a7
Willem Verbuyst <willemverbuyst@gmail.com>
2024-05-09 06:54:05 +0200

    Style card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,15 @@ import { formatName } from "../utils/name";
 
 export default function Card({ patient }: { patient: Patient }) {
   return (
-    <div>
+    <div
+      style={{
+        border: "2px solid white",
+        marginTop: "1rem",
+        borderRadius: "8px",
+        padding: "0.5rem",
+        minWidth: "500px",
+      }}
+    >
       <h3>{formatName(patient.name)}</h3>
       <p>{patient.birthDate}</p>
       <p>{patient.gender}</p>
